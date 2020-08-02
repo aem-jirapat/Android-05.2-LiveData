c457ca36b7bc622b332cd1d6c8698416cf8dde09
aem-jirapat <aemjirapat.tr@gmail.com>
2020-08-02 18:08:20 +0700

    UseLiveDataToDetectAGame

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -21,9 +21,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-/**
- * ViewModel containing all the logic needed to run the game
- */
+
 class GameViewModel : ViewModel() {
 
     private val _word = MutableLiveData<String>()
@@ -34,13 +32,14 @@ class GameViewModel : ViewModel() {
     val score: LiveData<Int>
         get() =_score
 
-    // The list of words - the front of the list is the next word to guess
+
     private lateinit var wordList: MutableList<String>
 
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
+
     private fun resetList() {
         wordList = mutableListOf(
                 "queen",
@@ -100,7 +99,13 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (!wordList.isEmpty()) {
+            onGameFinish()
+        }else{
             _word.value = wordList.removeAt(0)
         }
+    }
+
+    fun onGameFinish() {
+        _eventGameFinish.value = true
     }
 }
